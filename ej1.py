ad9335dc624068963731dbec2326895ee2bb7bcf
oliviaclemente <113981873+oliviaclemente@users.noreply.github.com>
2023-03-09 10:12:50 +0100

    cc

--- ej1.py
+++ ej1.py
@@ -22,3 +22,28 @@ class Polinomio:
         for term in self.terminos:
             valor += term[0] * x ** term[1]
         return valor
+
+import unittest
+
+class TestPolinomio(unittest.TestCase):
+    def setUp(self):
+        self.p = Polinomio()
+        self.p.agregar_termino(2, 3)
+        self.p.agregar_termino(1, 2)
+        self.p.agregar_termino(3, 0)
+
+    def test_eliminar_termino(self):
+        self.p.eliminar_termino(2)
+        self.assertEqual(len(self.p.terminos), 2)
+        self.assertFalse(self.p.existe_termino(2))
+
+    def test_existe_termino(self):
+        self.assertTrue(self.p.existe_termino(2))
+        self.assertFalse(self.p.existe_termino(1))
+
+    def test_obtener_valor(self):
+        self.assertEqual(self.p.obtener_valor(2), 28)
+        self.assertEqual(self.p.obtener_valor(0), 3)
+
+if __name__ == '__main__':
+    unittest.main()
